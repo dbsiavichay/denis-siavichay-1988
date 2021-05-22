d051d4194b0bdf4a636c184380180b3bc30f9f0a
Denis Siavichay <dsiavichay@mmorona.gob.ec>
2021-05-21 19:01:31 -0500

    :sparkles: Add admin models

--- formats/admin.py
+++ formats/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Currency
+from .models import Currency, Format
 
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("code", "name", "symbol")
+
+@admin.register(Format)
+class FormatAdmin(admin.ModelAdmin):
+    list_display = (
+        "country", "currency", "show", "display", "get_format"
+    )
--- formats/models.py
+++ formats/models.py
@@ -10,9 +10,42 @@ class Currency(models.Model):
     def __str__(self):
         return self.code
 
+    class Meta:
+        ordering = ("code",)
+
 
 class Format(models.Model):
+    class Show(models.IntegerChoices):
+        CODE = 1, "Code"
+        SYMBOL = 2, "Symbol"
+
+    class Display(models.IntegerChoices):
+        AFTER = 1, "After price"
+        BEFORE = 2, "Before price"
+
+    class Delimiter(models.TextChoices):
+        COMMA = ","
+        DOT = "."
+
     country = CountryField()
     currency = models.ForeignKey(
         Currency, on_delete=models.PROTECT,
     )
+    show = models.PositiveSmallIntegerField(
+        choices=Show.choices, default=Show.CODE
+    )
+    display = models.PositiveSmallIntegerField(
+        choices=Display.choices, default=Display.AFTER
+    )
+    cents = models.BooleanField(
+        verbose_name="show cents?"
+    )
+    thousand_delimiter = models.CharField(
+        max_length=2, choices=Delimiter.choices,
+        default=Delimiter.COMMA
+    )
+
+    def __str__(self):
+        return self.get_format()
+
+    
